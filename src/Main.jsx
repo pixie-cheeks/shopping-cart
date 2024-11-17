4b9cc7c8ce00d53c35b26165139f266694d13feb
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-11-17 12:17:05 +0530

    Set up routes

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './main.css';
+import routes from './components/Routes.jsx';
 
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter(routes)} />
   </StrictMode>,
 );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,5 @@
-import PropTypes from 'prop-types';
-
-function RenderName({ name }) {
-  return <div>{name}</div>;
-}
-
-RenderName.propTypes = {
-  name: PropTypes.string,
-};
-
-RenderName.defaultProps = {
-  name: 'Zach',
-};
-
 function App() {
-  return (
-    <>
-      <p>Hello World!</p>
-      <RenderName />
-    </>
-  );
+  return <p>Hello World!</p>;
 }
 
 export default App;
