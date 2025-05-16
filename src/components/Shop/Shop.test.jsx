63734a9f89807063dfc37b9e7125dc12d0ac50cb
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-16 12:43:59 +0530

    Fix a test throwing errors

--- src/components/Shop/Shop.test.jsx
+++ src/components/Shop/Shop.test.jsx
@@ -17,6 +17,9 @@ const fakeProduct = {
   id: 3,
   title: 'Mens Cotton Jacket',
   price: 55.99,
+  category: "men's clothing",
+  description:
+    'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
   image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
   rating: {
     rate: 4.7,
