40466ee9434a0c842875ac55427ed9b9df161146
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-17 10:19:33 +0530

    Add pay section at the end of the cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Navbar } from '../Navbar/Navbar.jsx';
 import {
   getCartItems,
   setCartItems as setCartItemsLocal,
 } from '../cart-storage.js';
 import { CartItemList } from './CartItemList.jsx';
+import styles from './cart.module.css';
+
+function PaySection({ totalAmount }) {
+  return (
+    <div className={styles.paySection}>
+      <p>Total: {totalAmount}</p>
+      <button type="button">Checkout & Pay</button>
+    </div>
+  );
+}
+
+PaySection.propTypes = { totalAmount: PropTypes.number.isRequired };
+
+const roundToTwoDecimalPlaces = (number) => {
+  const TWO_PLACES = 10 ** 2;
+  return Math.round(number * TWO_PLACES) / TWO_PLACES;
+};
 
 function Cart() {
   const [cartItems, setCartItemsState] = useState(() => getCartItems() ?? []);
@@ -12,6 +30,12 @@ function Cart() {
     setCartItemsLocal(givenCartItems);
     setCartItemsState(givenCartItems);
   };
+  const totalAmount = roundToTwoDecimalPlaces(
+    cartItems.reduce(
+      (total, { price, quantity }) => total + price * quantity,
+      0,
+    ),
+  ).toLocaleString();
 
   return (
     <>
@@ -19,7 +43,10 @@ function Cart() {
       <h1>Your Cart</h1>
       <main>
         {cartItems.length > 0 ? (
-          <CartItemList {...{ cartItems, setCartItems }} />
+          <>
+            <CartItemList {...{ cartItems, setCartItems }} />
+            <PaySection {...{ totalAmount }} />
+          </>
         ) : (
           <p>There are no items in the cart.</p>
         )}
