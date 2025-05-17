ed8a8281656be671a718a5f1f161ff4600735f11
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-17 13:11:08 +0530

    Savepoint

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router';
+import styles from './navbar.module.css';
 import { getCartItems, attachSetCartItems } from '../cart-storage.js';
 
 const getNumberOfCartItems = () =>
@@ -14,19 +15,21 @@ function Navbar() {
   });
 
   return (
-    <nav>
-      <div>Vanity</div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/shop">Shop</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart | {numOfCartItems}</Link>
-        </li>
-      </ul>
+    <nav className={styles.nav}>
+      <div className="page__container">
+        <div className={styles.nav_logo}>Vanity</div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart | {numOfCartItems}</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
